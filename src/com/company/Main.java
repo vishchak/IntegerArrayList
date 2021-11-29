ae7934fdaefecbb9c8f6e2ed750bc5457420f73c
Denis <denis.vishchak@gmail.com>
2021-11-29 15:27:06 +0200

    fixed;

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,13 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> lst = new ArrayList<>(10);
         for (int i = 0; i < 10; i++) {
-            lst.add((int) (Math.random()*10));
+            lst.add((int) (Math.random() * 10));
         }
         System.out.println(lst);
-        for (int i = 0; i < 2; i++) {
-            lst.remove(i);
-        }
+        lst.remove(0);
+        lst.remove(0);
         System.out.println(lst);
         lst.remove(lst.size() - 1);
         System.out.println(lst);
-
     }
 }
